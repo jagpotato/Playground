34e14b2c32f6648dd5ded7226f3ceba96972cd30
daiki <dik.piyo.piyo@gmail.com>
2018-09-12 01:40:05 +0900

    commit

--- Assets/Scripts/Audio/AudioCube.cs
+++ Assets/Scripts/Audio/AudioCube.cs
@@ -3,45 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class AudioCube : MonoBehaviour {
-	[SerializeField]
-	private AudioSource audioSource;
+public class AudioCube : MonoBehaviour
+{
+  [SerializeField]
+  private AudioSource audioSource;
 
-	private int sampleSize = 256;
+  private int sampleSize = 256;
 
-	private float[] sample;
+  private float[] sample;
 
-	float cubeScale = 0f;
-	
-	void Start () {
-		sample = new float[sampleSize];
-	}
-	
-	void Update () {
-		UpdateByOutput();
-		// UpdateBySpectrum();
-	}
+  float cubeScale = 0f;
 
-	private void UpdateByOutput () {
-		audioSource.GetOutputData(sample, 1);
-		var volume = sample.Select(x => x * x).Sum() / sample.Length;
-		Debug.Log(volume);
-		transform.localScale = Vector3.one + Vector3.one * volume * 100;
-	}
+	private AudioClip audioClip;
+	private float max;
 
-	private void UpdateBySpectrum () {
-		audioSource.GetSpectrumData(sample, 0, FFTWindow.BlackmanHarris);
-		var deltaFreq = AudioSettings.outputSampleRate / (sampleSize * 2);
-		// for (int i = 0; i < sampleSize; i++) {
-		// 	var freq = deltaFreq * i;
-		// 	if (freq <= 7350) cubeScale += sample[i];
+  void Start()
+  {
+    sample = new float[sampleSize];
+		audioClip = audioSource.clip;
+		var data = new float[audioClip.samples * audioClip.channels];
+		audioClip.GetData(data, 0);
+		Debug.Log(data.Max());
+		max = data.Max();
+		// foreach(float e in data)
+		// {
+		// 	Debug.Log(e);
 		// }
-		// cubeScale *= 10;
+  }
 
-		// Debug.Log(sample.Max());
-		float max = sample.Max();
-		if (max > 0.015) cubeScale = max * 20f;
-		else cubeScale = 0f;
-		transform.localScale = Vector3.one + Vector3.one * cubeScale;
+  void Update()
+  {
+    UpdateByOutput();
+    // UpdateBySpectrum();
+  }
+
+  private void UpdateByOutput()
+  {
+    audioSource.GetOutputData(sample, 1);
+		// if (max * 0.9 <= sample.Max()) Debug.Log("aaa");
+    var volume = sample.Select(x => x * x).Sum() / sample.Length;
+    // Debug.Log(volume);
+    // transform.localScale = Vector3.one + Vector3.one * volume * 100;
+		UpdateScale(volume * 10);
+  }
+
+  private void UpdateBySpectrum()
+  {
+    audioSource.GetSpectrumData(sample, 0, FFTWindow.BlackmanHarris);
+    var deltaFreq = AudioSettings.outputSampleRate / (sampleSize * 2);
+    // for (int i = 0; i < sampleSize; i++) {
+    // 	var freq = deltaFreq * i;
+    // 	if (freq <= 7350) cubeScale += sample[i];
+    // }
+    // cubeScale *= 10;
+
+    // Debug.Log(sample.Max());
+    float max = sample.Max();
+    if (max > 0.015) cubeScale = max * 20f;
+    else cubeScale = 0f;
+    transform.localScale = Vector3.one + Vector3.one * cubeScale;
+  }
+
+  private void UpdateScale(float value)
+	{
+		var scale = Mathf.Lerp(1, 2f, value);
+		transform.localScale = new Vector3(scale, scale, scale);
 	}
 }
---
+++ Assets/Scripts/Move/Move.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+	public GameObject sphere1, sphere2, sphere3;
+	public float speed;
+	private int time = 0;
+	private float angle = 0f;
+	private float radius = 10f;
+	private Vector3 pos1, pos2, pos3;
+	void Start () {
+		pos1 = Vector3.zero;
+		pos2 = Vector3.zero;
+		pos3 = Vector3.zero;
+	}
+	
+	void Update () {
+		// if (time < 100) {
+		// 	sphere1.transform.position += (transform.up + transform.right) * speed;
+		// } else if (time == 100) {
+		// 	sphere1.transform.position = new Vector3(0, 0, 0);
+		// }
+		// time++;
+		pos1.x = Mathf.Sin(angle) * radius;
+		pos1.z = Mathf.Cos(angle) * radius;
+		pos1.y += 0.001f;
+		sphere1.transform.position = pos1;
+
+		pos2.x = Mathf.Sin(angle - 0.3f) * radius;
+		pos2.z = Mathf.Cos(angle - 0.3f) * radius;
+		pos2.y = pos1.y - 0.1f;
+		sphere2.transform.position = pos2;
+
+		pos3.x = Mathf.Sin(angle - 0.6f) * radius;
+		pos3.z = Mathf.Cos(angle - 0.6f) * radius;
+		pos3.y = pos2.y - 0.1f;
+		sphere3.transform.position = pos3;
+
+		angle += 0.03f;
+		// if (time > 10) {
+		// 	pos2.x = Mathf.Sin(Time.time / 10 * speed * 10) * 10f;
+		//   pos2.z = Mathf.Cos(Time.time / 10 * speed * 10) * 10f;
+		//   pos2.y += 0.01f;
+		// 	sphere2.transform.position = pos2;
+		// }
+	}
+}
